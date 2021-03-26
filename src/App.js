0c155c0283f850d71cee3eb1d9eb2c8e88db7874
jmeade11 <hobokenjam@yahoo.com>
2021-03-26 03:30:58 -0400

    Remove App.css import

--- src/App.js
+++ src/App.js
@@ -1,21 +1,26 @@
-import { useState, useEffect } from 'react'
-import { Route } from 'react-router-dom'
-import './App.css';
-import Flavors from './components/Flavors'
-import FlavorID from './components/FlavorID'
-import Form from './components/Form'
-import Nav from './components/Nav'
+import { useState, useEffect } from 'react';
+import { Route } from 'react-router-dom';
+import Flavors from './components/Flavors';
+import FlavorID from './components/FlavorID';
+import Form from './components/Form';
+import Nav from './components/Nav';
 
 function App() {
-  const [flavors, setFlavors] = useState()
-
+  const [flavors, setFlavors] = useState();
 
   return (
     <div>
-        <Nav/>
-        <Route path="/" exact render={() => <Flavors flavors={flavors} setFlavors={setFlavors}/>}/>
-        <Route path="/add-flavor" component={Form}/>
-        <Route path="/flavors/:id" render={(routerProps) => <FlavorID match={routerProps.match}/>}/>
+      <Nav />
+      <Route
+        path="/"
+        exact
+        render={() => <Flavors flavors={flavors} setFlavors={setFlavors} />}
+      />
+      <Route path="/add-flavor" component={Form} />
+      <Route
+        path="/flavors/:id"
+        render={(routerProps) => <FlavorID match={routerProps.match} />}
+      />
     </div>
   );
 }
