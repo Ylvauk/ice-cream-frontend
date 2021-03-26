f6d0d9a463272b40d7148aa7aec31089343aa034
jmeade11 <hobokenjam@yahoo.com>
2021-03-26 04:06:05 -0400

    Fix form errors
    Mostly the changes involved cleaning up the component and
    renaming it internally to match the naming convention in
    App.js.

--- src/components/Flavor.js
+++ src/components/Flavor.js
@@ -1,80 +1,81 @@
-import React, { useState, useEffect} from 'react';
-import { useHistory, Link } from 'react-router-dom'
-import axios from 'axios'
+import React, { useState, useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
+import axios from 'axios';
 
-const FlavorID = ({ match }) => {
-    let history = useHistory()
+const Flavor = ({ match }) => {
+  const id = match.params.id;
 
-    const [flavor, setFlavor] = useState('')
-    const [modal, setModal] = useState(false)
+  const history = useHistory();
 
-    useEffect(() => {
-        const id = match.params.id
-        fetch(`http://localhost:3111/icecream/${id}`)
-            .then(res => res.json())
-            .then(res => setFlavor(res))
-    }, [match.params.id])
+  const [flavor, setFlavor] = useState(null);
+  const [modal, setModal] = useState(false);
 
-    const handleChange = (event) => {
-            setFlavor({...flavor, [event.target.name]: event.target.value})
+  useEffect(() => {
+    fetch(`http://localhost:3111/icecream/${id}`)
+      .then((res) => res.json())
+      .then((res) => setFlavor(res));
+  }, [id]);
 
-    }
+  const handleChange = (event) => {
+    setFlavor({ ...flavor, [event.target.id]: event.target.value });
+  };
 
-    const editShowPage = () => {
-        setModal(true)
-    }
+  const editShowPage = () => {
+    setModal(true);
+  };
 
-    const closeModal = () => {
-        setModal(false)
-    }
+  const closeModal = () => {
+    setModal(false);
+  };
 
-    const handleSubmit = (event) => {
-        event.preventDefault()
-        // Write your PUT fetch() or axios() request here
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // Write your PUT fetch() or axios() request here
 
+    history.push('/');
+  };
 
+  const handleDelete = () => {
+    // Write your DELETE fetch() or axios() request here
 
-        history.push('/')
-    }
+    history.push('/');
+  };
 
-    const handleDelete = () => {
-        // Write your DELETE fetch() or axios() request here
+  if (!flavor) {
+    return <h1>Loading...</h1>;
+  }
 
-
-        history.push('/')
-    }
-
-
-    if(!flavor){
-        return <h1>Loading...</h1>
-    }
-
-    return (
-        <div className="showID">
-            { modal ? 
-                <div>
-                    <div>
-                        <h2>Edit this show:</h2>
-                        <form onSubmit={handleSubmit}>
-                            <label for="flavor"/>
-                            <input onChange={handleChange} name="flavor" value={flavor.flavor}/>
-                            <label for="varieties"/>
-                            <input onChange={handleChange} name="varieties" value={flavor.varieties}/>
-                            <br/>
-                            <button type="submit">Submit</button>
-                        </form>
-                        <button onClick={closeModal}>Close</button>
-                    </div>
-                </div> 
-                : null
-            }
-            <h2>{flavor.flavor}</h2>
-            <h3>Varieties: {flavor.varieties}</h3>
-
-            <button onClick={editShowPage}>Edit</button>
-            <button onClick={handleDelete}>Delete</button>
+  return (
+    <section>
+      {modal ? (
+        <div className="modal">
+          <h2>Editing {flavor.flavor}</h2>
+          <form onSubmit={handleSubmit}>
+            <label htmlFor="flavor">Flavor</label>
+            <input onChange={handleChange} id="flavor" value={flavor.flavor} />
+            <label htmlFor="varieties">Varieties</label>
+            <input
+              onChange={handleChange}
+              id="varieties"
+              value={flavor.varieties}
+            />
+            <button type="submit">Submit</button>
+            <button type="button" onClick={closeModal}>
+              Close
+            </button>
+          </form>
         </div>
-    );
+      ) : (
+        <>
+          <h2>{flavor.flavor}</h2>
+          <h3>Varieties: {flavor.varieties}</h3>
+
+          <button onClick={editShowPage}>Edit</button>
+          <button onClick={handleDelete}>Delete</button>
+        </>
+      )}
+    </section>
+  );
 };
 
-export default FlavorID;
+export default Flavor;
