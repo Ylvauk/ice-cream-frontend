5adc3a1b285d3af75a0ac8cdc57a8e55c08c35e3
esin87 <esin.saribudak@gmail.com>
2022-01-20 21:58:22 -0600

    Update components for React Router v6

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,44 +1,44 @@
 import React, { useState } from 'react';
-import { useHistory } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
 
 const Form = () => {
-  const history = useHistory();
-  const [flavor, setFlavor] = useState({
-    flavor: '',
-    varieties: 0,
-  });
+	const navigate = useNavigate();
+	const [flavor, setFlavor] = useState({
+		flavor: '',
+		varieties: 0,
+	});
 
-  const handleChange = (event) => {
-    setFlavor({ ...flavor, [event.target.id]: event.target.value });
-  };
+	const handleChange = (event) => {
+		setFlavor({ ...flavor, [event.target.id]: event.target.value });
+	};
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    // Write your POST fetch() or axios() request here
+	const handleSubmit = (event) => {
+		event.preventDefault();
+		// Write your POST fetch() or axios() request here
 
-    history.push('/');
-  };
+		navigate('/');
+	};
 
-  return (
-    <form onSubmit={handleSubmit} className="create-form">
-      <label htmlFor="flavor">Flavor: </label>
-      <input
-        onChange={handleChange}
-        id="flavor"
-        value={flavor.flavor}
-        placeholder="Flavor"
-      />
-      <label htmlFor="varieties">Varieties: </label>
-      <input
-        onChange={handleChange}
-        id="varieties"
-        value={flavor.varieties}
-        placeholder="Varieties"
-      />
-      <button type="submit">Submit</button>
-    </form>
-  );
+	return (
+		<form onSubmit={handleSubmit} className='create-form'>
+			<label htmlFor='flavor'>Flavor: </label>
+			<input
+				onChange={handleChange}
+				id='flavor'
+				value={flavor.flavor}
+				placeholder='Flavor'
+			/>
+			<label htmlFor='varieties'>Varieties: </label>
+			<input
+				onChange={handleChange}
+				id='varieties'
+				value={flavor.varieties}
+				placeholder='Varieties'
+			/>
+			<button type='submit'>Submit</button>
+		</form>
+	);
 };
 
 export default Form;
